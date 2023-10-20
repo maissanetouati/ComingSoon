9e9e1e5cd95d291257d928d435c1007e8ee99b49
maissanetouati <77059362+maissanetouati@users.noreply.github.com>
2023-10-20 15:25:09 +0200

    ajout input

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,53 +14,57 @@ const IndexPage = () => (
       className="absolute w-full left-24 bottom-24 animate-blob2"
     />} */}
     <img src="images/logo-marbre.svg"
-    className="w-48 md:w-48 animate-blob2"
+    className="w-48 md:w-48"
     />
     <div className="relative z-10 py-6 space-y-16 lg:space-y-32 text-gray-900">
       <div className="text-center space-y-10 text-center space-y-6 md:space-y-8">
-        <h3 className="font-light font-sans text-black text-xl uppercase tracking-wider">
-          Nous revenons bientôt
-        </h3>
         <h1 className="text-4xl md:text-5xl lg:text-7xl font-bold font-sans text-black">EN MAINTENANCE</h1>
-        <p className="text-lg md:text-xl font-sans lg:text-2xl tracking-wide mx-4 md:mx-10 lg:max-w-4xl lg:mx-auto font-light">
+        <h3 className="font-bold font-sans text-black text-xl uppercase tracking-wider">
+          Nous revenons le 11 Novembre
+        </h3>
+
+        <p className="text-md md:text-lg font-sans tracking-wide font-light mb-0">
+              Soyez les premiers avertis de notre retour : 
+          </p>
+        <form className="relative z-10 mx-10 lg:max-w-xl lg:mx-auto">
+        <input
+          type="text"
+          placeholder="Entrez votre email"
+          className="w-full text-sm font-light text-gray-900 placeholder-gray-500 py-3 pl-3 pr-24 rounded-xl"
+        />
+      <button className="absolute top-1 right-1 bottom-1 px-4 lg:px-8 text-md font-medium bg-gray-900 text-white rounded-xl transition ease-in-out duration-500 hover:bg-red-500">
+      <img src="images/bell4.png" alt="Bell Icon" className="w-5 h-5"/>
+      </button>
+      </form>
+        <p className="text-md md:text-md font-sans lg:text-md tracking-wide mx-4 md:mx-10 lg:max-w-4xl lg:mx-auto font-light">
         LUXEDEFRANCE.FR est actuellement en maintenance pour vous offrir une expérience améliorée.  
         Merci pour votre patience et à très bientôt sur notre nouveau site.
         </p>
-        <p className="text-lg md:text-xl font-sans tracking-wide mx-4 md:mx-10 lg:max-w-xl lg:mx-auto font-bold">Veuillez noter que toutes vos commandes sont toujours traitées et expédiées sans délai. </p>
+        <p className="text-lg md:text-xl font-sans tracking-wide mx-4 md:mx-10 lg:max-w-xl lg:mx-auto font-bold">Veuillez noter que toutes les commandes déjà passées sont toujours traitées et expédiées sans délais. </p>
+
+
+        
+
         <div className="flex flex-col sm:flex-row justify-center items-center space-y-2 sm:space-y-0 sm:space-x-2">
           <p className="text-md md:text-lg font-sans tracking-wide font-light mb-0">
-              Vous pouvez toujours passer commande via Whatsapp
+              Nous contacter :
           </p>
-          <Link href="https://wa.me/+33603978674">
-              <span className="flex items-center justify-center w-16 h-16 rounded-full bg-gray-50 transition ease-in-out duration-500 hover:bg-red-500 hover:text-white hover:shadow-lg">
-                  <img src="/images/whatsapp.png" alt="WhatsApp" className="w-8 h-8" />
-              </span>
-          </Link>
+          
       </div>
 
       </div>
-      {/* <form className="relative z-10 mx-10 lg:max-w-xl lg:mx-auto">
-        <input
-          type="text"
-          placeholder="hi@themeptation.net"
-          className="w-full text-2xl font-light text-gray-900 placeholder-gray-500 py-5 pl-5 pr-36 lg:pr-44 rounded-xl"
-        />
-        <button className="absolute top-1 right-1 bottom-1 px-4 lg:px-10 text-xl font-semibold bg-gray-900 text-white rounded-xl transition ease-in-out duration-500 hover:bg-red-500">
-          Notify me
-        </button>
-      </form> */}
+      
+
     </div>
-    <p className="text-md md:text-lg font-sans tracking-wide font-light">Contactez-nous sur Snapchat ou par Mail</p>
 
     <div className="bottom-5">
   <ul className="flex space-x-3">
     <li>
-      <Link 
-        href="https://t.snapchat.com/dZ0hRisk"
-        className="flex items-center justify-center w-16 h-16 rounded-full bg-gray-50 transition ease-in-out duration-500 hover:bg-red-500 hover:text-white hover:shadow-lg"
-      >
-        <img src="/images/snap.png" alt="Snapchat" className="w-8  h-8" />
-      </Link>
+        <Link href="https://wa.me/+33603978674">
+            <span className="flex items-center justify-center w-16 h-16 rounded-full bg-gray-50 transition ease-in-out duration-500 hover:bg-red-500 hover:text-white hover:shadow-lg">
+                <img src="/images/whatsapp.png" alt="WhatsApp" className="w-8 h-8" />
+            </span>
+        </Link>
     </li>
     <li>
       <Link 
@@ -70,6 +74,15 @@ const IndexPage = () => (
         <img src="/images/mail.png" alt="Email us" className="w-8 h-8" />
       </Link>
     </li>
+    <li>
+      <Link 
+        href="https://t.snapchat.com/dZ0hRisk"
+        className="flex items-center justify-center w-16 h-16 rounded-full bg-gray-50 transition ease-in-out duration-500 hover:bg-red-500 hover:text-white hover:shadow-lg"
+      >
+        <img src="/images/snap.png" alt="Snapchat" className="w-8  h-8" />
+      </Link>
+    </li>
+    
   </ul>
 </div>
   </Layout>
